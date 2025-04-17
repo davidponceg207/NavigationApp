25a062d0fd64b6dda99fef489307fac4f8abff37
David <davidponceg207@gmail.com>
2025-04-17 12:57:17 -0600

    Back To Top

--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,12 +1,20 @@
 import { Text, View } from "react-native"
+import { globalStyles } from "../../theme/theme"
+import { PrimaryButton } from "../../components/shared/PrimaryButton"
+import { StackActions, useNavigation } from "@react-navigation/native"
 
 
 export const SettingsScreen = () => {
+
+  const navigator = useNavigation();
+
   return (
-    <View>
-        <Text>
-            SettingsScreen
-        </Text>
+    <View style={ globalStyles.container }>
+        <Text style={{marginBottom: 10}}>SettingsScreen</Text>
+
+        <PrimaryButton label="Return" onPress={() => navigator.goBack()} />
+
+        <PrimaryButton label="Home" onPress={() => navigator.dispatch(StackActions.popToTop())} />
     </View>
   )
 }

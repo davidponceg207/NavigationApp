ab2a3709d57af730e510b21fd84bee2bd8543a22
David <davidponceg207@gmail.com>
2025-07-12 16:28:36 -0600

    Add Toggle Drawer

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,13 +1,28 @@
-import { View } from "react-native"
+import { Pressable, View } from "react-native"
 import { globalStyles } from "../../theme/theme"
 import { PrimaryButton } from "../../components/shared/PrimaryButton"
-import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { DrawerActions, NavigationProp, useNavigation } from "@react-navigation/native";
 import { RootStackParams } from "../../routes/StackNavigator";
+import { useEffect } from "react";
+import { Text } from "react-native-gesture-handler";
 
 
 export const HomeScreen = () => {
 
   const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
+  useEffect(() => {
+
+    navigation.setOptions({
+      headerLeft: () => (
+        <Pressable onPress={() => navigation.dispatch( DrawerActions.toggleDrawer )}>
+          <Text>Menu</Text>
+        </Pressable>
+      )
+    });
+  
+  }, [])
+  
 
   return (
     <View style={globalStyles.container}>

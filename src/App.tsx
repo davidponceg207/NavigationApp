c9c7a3fedaee118b77a7db1b27d000ba69b872cc
David <davidponceg207@gmail.com>
2025-07-14 11:58:25 -0600

    Hamburguer Menu

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Text, View } from "react-native"
 import { StackNavigator } from './presentation/routes/StackNavigator';
 import { SideMenuNavigator } from './presentation/routes/SideMenuNavigator';
 import { BottomTabNavigator } from './presentation/routes/BottomTabNavigator';
+import { HamburgerMenu } from './presentation/components/shared/HamburgerMenu';
 
 
 export const App = () => {

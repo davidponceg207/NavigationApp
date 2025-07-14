653af148a6a73de0c0e803447ea40d4a8413d157
David <davidponceg207@gmail.com>
2025-07-13 20:58:22 -0600

    Bottom Tab Navigator Custom

--- src/presentation/routes/BottomTabNavigator.tsx
+++ src/presentation/routes/BottomTabNavigator.tsx
@@ -2,15 +2,34 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Tab1Screen } from '../screens/tabs/Tab1Screen';
 import { Tab2Screen } from '../screens/tabs/Tab2Screen';
 import { Tab3Screen } from '../screens/tabs/Tab3Screen';
+import { globalColors } from '../theme/theme';
+import { Text } from 'react-native-gesture-handler';
 
 const Tab = createBottomTabNavigator();
 
 export const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Tab1" component={Tab1Screen} />
-      <Tab.Screen name="Tab2" component={Tab2Screen} />
-      <Tab.Screen name="Tab3" component={Tab3Screen} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarLabelStyle: {
+          marginBottom: 5
+        },
+        sceneStyle: {
+          backgroundColor: globalColors.background
+        },
+        headerStyle: {
+          elevation: 0,
+          shadowColor: 'transparent'
+        },
+        tabBarStyle: {
+          borderTopWidth: 0,
+          elevation: 0
+        }
+      }}
+    >
+      <Tab.Screen name="Tab1" options={{ title: 'Tab1', tabBarIcon: ({ color }) => (<Text style={{ color }} >Tab1</Text>) }} component={Tab1Screen} />
+      <Tab.Screen name="Tab2" options={{ title: 'Tab2', tabBarIcon: ({ color }) => (<Text style={{ color }} >Tab2</Text>) }} component={Tab2Screen} />
+      <Tab.Screen name="Tab3" options={{ title: 'Tab3', tabBarIcon: ({ color }) => (<Text style={{ color }} >Tab3</Text>) }} component={Tab3Screen} />
     </Tab.Navigator>
   );
 }
